74b0829b239291345590e15d7ea76af6d7e0a949
Ivan Bachyla <ivan.bachyla@outlook.com>
2024-02-12 19:50:10 +0100

    Init project

---
+++ src/main/java/com/github/ibachyla/chleb/ChlebApplication.java
@@ -0,0 +1,13 @@
+package com.github.ibachyla.chleb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChlebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChlebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/ibachyla/chleb/ChlebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ibachyla.chleb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChlebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
